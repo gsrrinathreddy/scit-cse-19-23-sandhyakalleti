becbac5fbad2a29f99d5a16f2c04653387bcee96
sandhyakelleti <sandhyakalleti4@gmail.com>
2023-03-31 10:09:18 +0530

    project1 added

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import { Routes,Route } from 'react-router-dom';
+import INavbar from './components/INavbar';
+import Aboutus from './pages/Aboutus';
+import Experience from './pages/Experience';
+import Hobbies from './pages/Hobbies';
+import Qualification from './pages/Qualification';
+import Skills from './pages/Skills';
+import { Card } from '@mui/material';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className='App'>
+      <INavbar/>
+      <Routes>
+        <Route path="Aboutus"element ={<Aboutus/>}/>
+        <Route path="Experience"element ={<Experience/>}/>
+        <Route path="Hobbies"element ={<Hobbies/>}/>
+        <Route path="Qualification"element ={<Qualification/>}/>
+        <Route path="Skills"element ={<Skills/>}/>
+     </Routes>
+      
     </div>
   );
 }
---
+++ project1/src/pages/Aboutus/index.js
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material';
+
+export default function About(){
+    return(
+        <>
+<Typography>Name: Kelleti Sandhya</Typography>
+<Typography>Father Name: Kelleti Mallesham</Typography>
+<Typography>Mother Name: Kelleti Manga</Typography>
+<Typography>Email: sandhyakalleti4@gmail.com</Typography>
+<Typography>Phone No.: 8186018041</Typography>
+        </>
+        
+    )
+}
